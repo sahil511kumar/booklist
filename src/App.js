50dbe20092a2464d1744be7ac78806dbd6d1115c
Sahil Kumar <sahil511kumar@gmail.com>
2020-09-29 23:29:00 +0530

    booklist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BookContextProvider from './contexts/bookcontext';
+import Navbar from './components/navbar';
+import BookList from './components/booklist';
+import BookForm from './components/bookform';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookContextProvider>
+        <Navbar/>
+        <BookList/>
+        <BookForm/>
+      </BookContextProvider>
     </div>
   );
 }
